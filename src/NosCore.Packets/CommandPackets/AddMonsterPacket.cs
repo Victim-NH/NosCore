cee71eb86f86d08a7240a0f183a8d204c7c6f381
NosHamlet - Team <39097430+Victim-NH@users.noreply.github.com>
2018-05-08 18:56:58 +0200

    Insert AddMonster Packet

---
+++ src/NosCore.Packets/CommandPackets/AddMonsterPacket.cs
@@ -0,0 +1,24 @@
+using OpenNos.Core;
+using OpenNos.Domain;
+
+namespace OpenNos.GameObject.CommandPackets
+{
+    [PacketHeader("$AddMonster", PassNonParseablePacket = true, Authority = AuthorityType.GameMaster)]
+    public class AddMonsterPacket : PacketDefinition
+    {
+        #region Properties
+
+        [PacketIndex(0)]
+        public short MonsterVNum { get; set; }
+
+        [PacketIndex(1)]
+        public bool IsMoving { get; set; }
+
+        public static string ReturnHelp()
+        {
+            return "$AddMonster VNUM MOVE";
+        }
+
+        #endregion
+    }
+}
